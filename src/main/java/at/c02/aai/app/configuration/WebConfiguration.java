4a662b0d858771de79637a6e684481dac9b59730
Daniel Hartl <daniel_hartl@gmx.at>
2018-03-13 15:34:24 +0100

    remove thymeleaf configuration

--- src/main/java/at/c02/aai/app/configuration/WebConfiguration.java
+++ src/main/java/at/c02/aai/app/configuration/WebConfiguration.java
@@ -3,25 +3,19 @@ package at.c02.aai.app.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public CommonsRequestLoggingFilter requestLoggingFilter() {
-	CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-	loggingFilter.setIncludeClientInfo(true);
-	loggingFilter.setIncludeQueryString(true);
-	loggingFilter.setIncludePayload(true);
-	loggingFilter.setIncludeHeaders(false);
-	return loggingFilter;
-    }
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(false);
+		return loggingFilter;
+	}
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
-	registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-    }
 }
--- src/main/resources/static/js/map.js
+++
@@ -1,8 +0,0 @@
-
-var mymap = L.map('mapid').setView([47.069241, 15.438473], 13);
-L.tileLayer('https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png', {
-    attribution: 'Tiles &copy; <a href="//www.basemap.at/">basemap.at</a> (STANDARD)',
-    subdomains: ['maps1','maps2','maps3','maps4'],
-    maxZoom: 18,
-    id: 'basemap.normal',
-}).addTo(mymap);
