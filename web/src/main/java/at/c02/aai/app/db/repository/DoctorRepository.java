35d40e508aaa80f8c37c652f7735dc379f77c1c8
Daniel Hartl <daniel_hartl@gmx.at>
2018-03-30 15:41:34 +0200

    display doctors in map

--- web/src/main/java/at/c02/aai/app/db/repository/DoctorRepository.java
+++ web/src/main/java/at/c02/aai/app/db/repository/DoctorRepository.java
@@ -1,9 +1,17 @@
 package at.c02.aai.app.db.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import at.c02.aai.app.db.entity.Doctor;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+	@EntityGraph(attributePaths = { "facility" })
+	@Query("select doctor from Doctor doctor")
+	List<Doctor> findAllWithFacility();
 
 }
--- web/src/main/java/at/c02/aai/app/web/ui/VaadinUI.java
+++ web/src/main/java/at/c02/aai/app/web/ui/VaadinUI.java
@@ -1,24 +1,55 @@
 package at.c02.aai.app.web.ui;
 
-import org.vaadin.addon.leaflet.LMap;
-import org.vaadin.addon.leaflet.LTileLayer;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.addon.leaflet.LLayerGroup;
+import org.vaadin.addon.leaflet.LMap;
+import org.vaadin.addon.leaflet.LMarker;
+import org.vaadin.addon.leaflet.LTileLayer;
+import org.vaadin.addon.leaflet.shared.Point;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
 
+import at.c02.aai.app.db.entity.Doctor;
+import at.c02.aai.app.db.repository.DoctorRepository;
+
 @SpringUI(path = "/ui")
+@Theme("aai")
 public class VaadinUI extends UI {
 
-    private static final long serialVersionUID = 1L;
+	@Autowired
+	private DoctorRepository doctorRepository;
 
-    @Override
-    protected void init(VaadinRequest request) {
-	LMap map = new LMap();
-	LTileLayer basemapLayer = new LBasemapLayer();
-	map.addBaseLayer(basemapLayer, "Basemap");
-	map.setView(47.069241d, 15.438473d, 13d);
-	setContent(map);
+	private static final long serialVersionUID = 1L;
 
-    }
+	@Override
+	protected void init(VaadinRequest request) {
+		LMap map = new LMap();
+		LTileLayer basemapLayer = new LBasemapLayer();
+		map.addBaseLayer(basemapLayer, "Basemap");
+		map.setView(47.069241d, 15.438473d, 13d);
+		LLayerGroup doctorMarkerGroup = new LLayerGroup();
+		List<Doctor> doctors = doctorRepository.findAllWithFacility();
+		doctors.stream().map(this::createMarker).forEach(doctorMarkerGroup::addComponent);
+		map.addLayer(doctorMarkerGroup);
+		setContent(map);
+
+	}
+
+	private LMarker createMarker(Doctor doctor) {
+		if(doctor.getFacility().getGeoLat() == null || doctor.getFacility().getGeoLon() == null) {
+			return null;
+		}
+		LMarker marker = new LMarker(new Point(doctor.getFacility().getGeoLat().doubleValue(),
+				doctor.getFacility().getGeoLon().doubleValue()));
+		marker.setIcon(new ThemeResource("images/doctor.png"));
+		marker.setPopup(doctor.getFacility().getTitle());
+		marker.setIconAnchor(new Point(16, 37));
+		return marker;
+	}
 }
