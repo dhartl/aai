d8cdefacc853c9b94fdd7065282749081b57ebe5
Daniel Hartl <daniel_hartl@gmx.at>
2018-04-01 17:19:46 +0200

    fix baseentity hashcode for null ids

--- web/src/main/java/at/c02/aai/app/db/entity/BaseEntity.java
+++ web/src/main/java/at/c02/aai/app/db/entity/BaseEntity.java
@@ -9,6 +9,9 @@ public abstract class BaseEntity {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
+		if (getId() == null) {
+			return prime;
+		}
 		int result = 1;
 		result = prime * result + (int) (getId() ^ (getId() >>> 32));
 		return result;
