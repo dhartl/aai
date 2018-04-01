bcf1948a0379f762b60d459bae75aa3233797148
Daniel Hartl <daniel_hartl@gmx.at>
2018-04-01 13:57:59 +0200

    reset database

---
+++ web/database/reset_database.sql
@@ -0,0 +1,8 @@
+delete from doctorInsurance;
+delete from doctorSpeciality;
+delete from insurance;
+delete from speciality;
+delete from doctor;
+delete from pharmacy;
+delete from hours;
+delete from facility;
