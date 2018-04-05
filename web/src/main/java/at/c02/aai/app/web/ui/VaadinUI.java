7f0cd0c8b19f53993307f2191c38e5835fcc6e90
Daniel Hartl <daniel_hartl@gmx.at>
2018-04-05 20:57:25 +0200

    doctors/pharmacies without location are not displayed in ui

--- web/src/main/java/at/c02/aai/app/web/ui/VaadinUI.java
+++ web/src/main/java/at/c02/aai/app/web/ui/VaadinUI.java
@@ -3,6 +3,7 @@ package at.c02.aai.app.web.ui;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -33,104 +34,104 @@ import at.c02.aai.app.service.bean.DistanceBean;
 @Theme("aai")
 public class VaadinUI extends UI {
 
-	private static final Logger logger = LoggerFactory.getLogger(VaadinUI.class);
+    private static final Logger logger = LoggerFactory.getLogger(VaadinUI.class);
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Autowired
-	private FacilityRepository facilityRepository;
+    @Autowired
+    private FacilityRepository facilityRepository;
 
-	@Autowired
-	private DistanceService distanceService;
+    @Autowired
+    private DistanceService distanceService;
 
-	@Override
-	protected void init(VaadinRequest request) {
-		LMap map = new LMap();
-		LTileLayer basemapLayer = new LBasemapLayer();
-		map.addBaseLayer(basemapLayer, "Basemap");
-		map.setView(47.069241d, 15.438473d, 13d);
-		LMarkerClusterGroup markerGroup = new LMarkerClusterGroup();
-		markerGroup.setDisableClusteringAtZoom(13);
-		createDoctorMarkers(markerGroup);
-		createPharmacyMarkers(markerGroup);
-		map.addLayer(markerGroup);
-		// createHeatmap(map, getDoctorPoints());
+    @Override
+    protected void init(VaadinRequest request) {
+	LMap map = new LMap();
+	LTileLayer basemapLayer = new LBasemapLayer();
+	map.addBaseLayer(basemapLayer, "Basemap");
+	map.setView(47.069241d, 15.438473d, 13d);
+	LMarkerClusterGroup markerGroup = new LMarkerClusterGroup();
+	markerGroup.setDisableClusteringAtZoom(13);
+	createDoctorMarkers(markerGroup);
+	createPharmacyMarkers(markerGroup);
+	map.addLayer(markerGroup);
+	// createHeatmap(map, getDoctorPoints());
 
-		createHeatmap(map, getPharmaciesDoctorPoints());
-		setContent(map);
+	createHeatmap(map, getPharmaciesDoctorPoints());
+	setContent(map);
 
+    }
+
+    private void createDoctorMarkers(LMarkerClusterGroup markerGroup) {
+	List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.DOCTOR);
+	doctors.stream().map(this::createMarker).filter(Objects::nonNull).forEach(markerGroup::addComponent);
+    }
+
+    private void createPharmacyMarkers(LMarkerClusterGroup markerGroup) {
+	List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.PHARMACY);
+	doctors.stream().map(this::createMarker).filter(Objects::nonNull).forEach(markerGroup::addComponent);
+    }
+
+    private void createHeatmap(LMap map, Point[] points) {
+
+	LHeatMapLayer heatmapLayer = new LHeatMapLayer();
+	heatmapLayer.setPoints(points);
+	HashMap<Double, String> gradient = new LinkedHashMap<>();
+	gradient.put(0.4, "blue");
+	gradient.put(0.65, "lime");
+	gradient.put(1.0, "red");
+
+	heatmapLayer.setGradient(gradient);
+	heatmapLayer.setMinOpacity(.1);
+	heatmapLayer.setRadius(50);
+	heatmapLayer.setBlur(75);
+	heatmapLayer.setMaxZoom(14);
+
+	map.addLayer(heatmapLayer);
+    }
+
+    private Point[] getDoctorPoints() {
+	logger.debug("before requesting distances for heatmap");
+	List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.DOCTOR);
+	List<DistanceBean> distances = distanceService.createDistances(doctors, doctors, 1000);
+	logger.debug("found {} distances for heatmap", distances.size());
+	List<Point3D> points = distances.stream().map(distance -> new Point3D(distance.getGeoLat().doubleValue(),
+		distance.getGeoLon().doubleValue(), distance.getCount() / 10d)).collect(Collectors.toList());
+	return points.toArray(new Point[0]);
+    }
+
+    private Point[] getPharmaciesDoctorPoints() {
+	logger.debug("before requesting distances for heatmap");
+	List<Facility> pharmacies = facilityRepository.findByFacilityType(FacilityType.PHARMACY);
+	List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.DOCTOR);
+
+	List<DistanceBean> distances = distanceService.createDistances(pharmacies, doctors, 1500);
+	logger.debug("found {} distances for heatmap", distances.size());
+	List<Point3D> points = distances.stream().map(distance -> new Point3D(distance.getGeoLat().doubleValue(),
+		distance.getGeoLon().doubleValue(), distance.getCount() / 5d)).collect(Collectors.toList());
+	return points.toArray(new Point[0]);
+    }
+
+    private LMarker createMarker(Facility facility) {
+	if (facility.getGeoLat() == null || facility.getGeoLon() == null) {
+	    return null;
 	}
+	LMarker marker = new LMarker(new Point(facility.getGeoLat().doubleValue(), facility.getGeoLon().doubleValue()));
+	marker.setIcon(getFacilityImage(facility.getFacilityType()));
+	marker.setPopup(facility.getTitle());
+	marker.setIconAnchor(new Point(16, 37));
+	return marker;
+    }
 
-	private void createDoctorMarkers(LMarkerClusterGroup markerGroup) {
-		List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.DOCTOR);
-		doctors.stream().map(this::createMarker).forEach(markerGroup::addComponent);
+    private Resource getFacilityImage(FacilityType facilityType) {
+	switch (facilityType) {
+	case DOCTOR:
+	    return new ThemeResource("images/doctor.png");
+	case PHARMACY:
+	    return new ThemeResource("images/pharmacy.png");
+	default:
+	    break;
 	}
-
-	private void createPharmacyMarkers(LMarkerClusterGroup markerGroup) {
-		List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.PHARMACY);
-		doctors.stream().map(this::createMarker).forEach(markerGroup::addComponent);
-	}
-
-	private void createHeatmap(LMap map, Point[] points) {
-
-		LHeatMapLayer heatmapLayer = new LHeatMapLayer();
-		heatmapLayer.setPoints(points);
-		HashMap<Double, String> gradient = new LinkedHashMap<>();
-		gradient.put(0.4, "blue");
-		gradient.put(0.65, "lime");
-		gradient.put(1.0, "red");
-
-		heatmapLayer.setGradient(gradient);
-		heatmapLayer.setMinOpacity(.1);
-		heatmapLayer.setRadius(50);
-		heatmapLayer.setBlur(75);
-		heatmapLayer.setMaxZoom(14);
-
-		map.addLayer(heatmapLayer);
-	}
-
-	private Point[] getDoctorPoints() {
-		logger.debug("before requesting distances for heatmap");
-		List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.DOCTOR);
-		List<DistanceBean> distances = distanceService.createDistances(doctors, doctors, 1000);
-		logger.debug("found {} distances for heatmap", distances.size());
-		List<Point3D> points = distances.stream().map(distance -> new Point3D(distance.getGeoLat().doubleValue(),
-				distance.getGeoLon().doubleValue(), distance.getCount() / 10d)).collect(Collectors.toList());
-		return points.toArray(new Point[0]);
-	}
-
-	private Point[] getPharmaciesDoctorPoints() {
-		logger.debug("before requesting distances for heatmap");
-		List<Facility> pharmacies = facilityRepository.findByFacilityType(FacilityType.PHARMACY);
-		List<Facility> doctors = facilityRepository.findByFacilityType(FacilityType.DOCTOR);
-
-		List<DistanceBean> distances = distanceService.createDistances(pharmacies, doctors, 1500);
-		logger.debug("found {} distances for heatmap", distances.size());
-		List<Point3D> points = distances.stream().map(distance -> new Point3D(distance.getGeoLat().doubleValue(),
-				distance.getGeoLon().doubleValue(), distance.getCount() / 5d)).collect(Collectors.toList());
-		return points.toArray(new Point[0]);
-	}
-
-	private LMarker createMarker(Facility facility) {
-		if (facility.getGeoLat() == null || facility.getGeoLon() == null) {
-			return null;
-		}
-		LMarker marker = new LMarker(new Point(facility.getGeoLat().doubleValue(), facility.getGeoLon().doubleValue()));
-		marker.setIcon(getFacilityImage(facility.getFacilityType()));
-		marker.setPopup(facility.getTitle());
-		marker.setIconAnchor(new Point(16, 37));
-		return marker;
-	}
-
-	private Resource getFacilityImage(FacilityType facilityType) {
-		switch (facilityType) {
-		case DOCTOR:
-			return new ThemeResource("images/doctor.png");
-		case PHARMACY:
-			return new ThemeResource("images/pharmacy.png");
-		default:
-			break;
-		}
-		return null;
-	}
+	return null;
+    }
 }
