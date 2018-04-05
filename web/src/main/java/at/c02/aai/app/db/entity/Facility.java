fdbceed69e18260510ff9760b54f171f089503a2
Daniel Hartl <daniel_hartl@gmx.at>
2018-04-05 20:33:45 +0200

    add hourstotal to facility

--- web/src/main/java/at/c02/aai/app/db/entity/Facility.java
+++ web/src/main/java/at/c02/aai/app/db/entity/Facility.java
@@ -36,8 +36,8 @@ public class Facility extends BaseEntity {
     private String zipCode;
     @Column(name = "city", length = 255)
     private String city;
-	@Column(name = "state", length = 255)
-	private String state;
+    @Column(name = "state", length = 255)
+    private String state;
     /**
      * Geo-Latitude WGS84
      */
@@ -49,135 +49,143 @@ public class Facility extends BaseEntity {
     @Column(name = "geoLon")
     private BigDecimal geoLon;
 
-	@Column(name = "telephoneNr", length = 100)
-	private String telephoneNr;
-	@Column(name = "email", length = 255)
-	private String email;
-	@Column(name = "url", length = 255)
-	private String url;
-	@Column(name = "srcUrl", length = 255)
-	private String srcUrl;
-	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-	@JoinColumn(name = "facilityId")
-	private Set<Hours> hours = new LinkedHashSet<>(0);
+    @Column(name = "telephoneNr", length = 100)
+    private String telephoneNr;
+    @Column(name = "email", length = 255)
+    private String email;
+    @Column(name = "url", length = 255)
+    private String url;
+    @Column(name = "srcUrl", length = 255)
+    private String srcUrl;
+    @Column(name = "hoursTotal")
+    private Double hoursTotal;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "facilityId")
+    private Set<Hours> hours = new LinkedHashSet<>(0);
 
-	@Override
-	public Long getId() {
-		return getFacilityId();
-	}
+    @Override
+    public Long getId() {
+	return getFacilityId();
+    }
 
-	public Long getFacilityId() {
-		return facilityId;
-	}
+    public Long getFacilityId() {
+	return facilityId;
+    }
 
-	public void setFacilityId(Long facilityId) {
-		this.facilityId = facilityId;
-	}
+    public void setFacilityId(Long facilityId) {
+	this.facilityId = facilityId;
+    }
 
-	public FacilityType getFacilityType() {
-		return facilityType;
-	}
+    public FacilityType getFacilityType() {
+	return facilityType;
+    }
 
-	public void setFacilityType(FacilityType facilityType) {
-		this.facilityType = facilityType;
-	}
+    public void setFacilityType(FacilityType facilityType) {
+	this.facilityType = facilityType;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+	return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+	this.title = title;
+    }
 
-	public String getStreet() {
-		return street;
-	}
+    public String getStreet() {
+	return street;
+    }
 
-	public void setStreet(String street) {
-		this.street = street;
-	}
+    public void setStreet(String street) {
+	this.street = street;
+    }
 
-	public String getZipCode() {
-		return zipCode;
-	}
+    public String getZipCode() {
+	return zipCode;
+    }
 
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
+    public void setZipCode(String zipCode) {
+	this.zipCode = zipCode;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+	return city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+	this.city = city;
+    }
 
-	public String getState() {
-		return state;
-	}
+    public String getState() {
+	return state;
+    }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+    public void setState(String state) {
+	this.state = state;
+    }
 
-	public BigDecimal getGeoLat() {
-		return geoLat;
-	}
+    public BigDecimal getGeoLat() {
+	return geoLat;
+    }
 
-	public void setGeoLat(BigDecimal geoLat) {
-		this.geoLat = geoLat;
-	}
+    public void setGeoLat(BigDecimal geoLat) {
+	this.geoLat = geoLat;
+    }
 
-	public BigDecimal getGeoLon() {
-		return geoLon;
-	}
+    public BigDecimal getGeoLon() {
+	return geoLon;
+    }
 
-	public void setGeoLon(BigDecimal geoLon) {
-		this.geoLon = geoLon;
-	}
+    public void setGeoLon(BigDecimal geoLon) {
+	this.geoLon = geoLon;
+    }
 
-	public String getTelephoneNr() {
-		return telephoneNr;
-	}
+    public String getTelephoneNr() {
+	return telephoneNr;
+    }
 
-	public void setTelephoneNr(String telephoneNr) {
-		this.telephoneNr = telephoneNr;
-	}
+    public void setTelephoneNr(String telephoneNr) {
+	this.telephoneNr = telephoneNr;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+	return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+	this.email = email;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+	return url;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) {
+	this.url = url;
+    }
 
-	public String getSrcUrl() {
-		return srcUrl;
-	}
+    public String getSrcUrl() {
+	return srcUrl;
+    }
 
-	public void setSrcUrl(String srcUrl) {
-		this.srcUrl = srcUrl;
-	}
+    public void setSrcUrl(String srcUrl) {
+	this.srcUrl = srcUrl;
+    }
 
-	public Set<Hours> getHours() {
-		return hours;
-	}
+    public Double getHoursTotal() {
+	return hoursTotal;
+    }
 
-	public void setHours(Set<Hours> hours) {
-		this.hours = hours;
-	}
+    public void setHoursTotal(Double hoursTotal) {
+	this.hoursTotal = hoursTotal;
+    }
 
+    public Set<Hours> getHours() {
+	return hours;
+    }
 
+    public void setHours(Set<Hours> hours) {
+	this.hours = hours;
+    }
 
 }
