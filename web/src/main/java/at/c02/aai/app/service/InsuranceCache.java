98d6ec2fd01e65e2e1b78a1aeb1d3399272c75e6
Daniel Hartl <daniel_hartl@gmx.at>
2018-04-05 23:34:57 +0200

    fix insurance assignment

--- web/src/main/java/at/c02/aai/app/service/InsuranceCache.java
+++ web/src/main/java/at/c02/aai/app/service/InsuranceCache.java
@@ -9,32 +9,32 @@ import at.c02.aai.app.db.entity.Insurance;
 import at.c02.aai.app.db.repository.InsuranceRepository;
 
 public class InsuranceCache {
-	private Map<String, Insurance> insurances = new HashMap<>();
+    private Map<String, Insurance> insurances = new HashMap<>();
 
-	public InsuranceCache(InsuranceRepository insuranceRepository) {
-		super();
-		insuranceRepository.findAll().forEach(this::addInsurance);
-	}
+    public InsuranceCache(InsuranceRepository insuranceRepository) {
+	super();
+	insuranceRepository.findAll().forEach(this::addInsurance);
+    }
 
-	public Insurance findOrCreate(String code) {
-		String key = getKey(code);
-		if (key != null) {
-			return insurances.get(key);
-		}
-		return null;
+    public Insurance findOrCreate(String code) {
+	String key = getKey(code);
+	if (key != null) {
+	    return insurances.get(key);
 	}
+	return null;
+    }
 
-	private void addInsurance(Insurance insurance) {
-		String key = getKey(insurance.getName());
-		if (key != null) {
-			insurances.put(key, insurance);
-		}
+    private void addInsurance(Insurance insurance) {
+	String key = getKey(insurance.getCode());
+	if (key != null) {
+	    insurances.put(key, insurance);
 	}
+    }
 
-	private String getKey(String name) {
-		if (name == null) {
-			return null;
-		}
-		return StringUtils.trimToNull(name.toLowerCase());
+    private String getKey(String name) {
+	if (name == null) {
+	    return null;
 	}
+	return StringUtils.trimToNull(name.toLowerCase());
+    }
 }
